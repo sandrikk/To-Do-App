44599290ba3155ea05060c8c1044270ebf48e329
sandrik <krevovasandra@gmail.com>
2021-08-24 10:35:11 +0200

    add header

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+      <h2>To-do list</h2>
+  </header>
+  <article>
+    <input v-model="newTask" placeholder="Enter task" type="text">
+    <button class="add-button" @click="addTask">Add Task</button>
+    <ul class="todo-list">
+      <li class="li-task" v-for="(task, index) in tasks" :key="task">
+        <input class="checkbox" type="checkbox">
+        {{ task }}
+        <button class="delete-button" @click="removeTask(index)">X</button>
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script>
+  export default{
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.newTask)
+      this.newTask = ''
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
